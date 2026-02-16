410d83201c15ac845bf81fbaab86ec18286fc7cb
Gurmeet499 <gurmeetbajaj4@gmail.com>
2026-02-16 23:23:20 +0500

    Create drinks.cpp

---
+++ drinks.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int n,a[110];
+     cin>>n;
+      double ans = 0.0;
+    double sum =0.0;
+     for(int i=0;i<n;i++){
+      cin>>a[i];
+      sum += a[i];
+   
+
+     }
+    
+     
+    ans = sum/n;
+
+    cout<<fixed<<setprecision(12)<<ans<<"\n";
+
+
+
+    return 0;
+}
